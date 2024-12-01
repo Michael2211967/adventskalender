3d948cd3cc2667a044605e5d4606db88697d9bec
Michael Laubach <MLaubach@t-online.de>
2024-12-01 15:46:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function door1(div){
+    div.innerHTML = "Nimm Dir etwas Ruhe und Du wirst mehr Energie haben";
+    div.style.fontSize = '13px';
+    div.style.backgroundColor = 'red';
+}
+
+function door2(div){
+    div.innerHTML = "Spass muss sein, sprach Wallenstein und schob die Nüsse mit hinein";
+    div.style.fontSize = "13px";
+    div.style.backgroundColor = "yellow";
+}
+
+function door3(div){
+    div.innerHTML = "Die Ampel ist tot, es lebe die Ampel";
+    div.style.fontSize = "13px";
+    div.style.backgroundColor = "green";
+}
+
+function door4(div){
+    div.innerHTML = "Wir sind und bleiben Kinder unserer Eltern";
+    div.style.fontSize = "13px";
+    div.style.backgroundColor = "orange";
+}
+
+function door5(div){
+    div.innerHTML = "Der Clown kann bleiben, aber der Ferengi im Gorilla-Kostüm muß gehen";
+    div.style.fontSize = "13px";
+    div.style.backgroundColor = "khaki";
+}
+
+function door6(div){
+    div.innerHTML = "Sprechen Sie nur für sich, ich habe vor ewig zu leben";
+    div.style.fontSize = "13px";
+    div.style.backgroundColor = "aquamarine";
+}
